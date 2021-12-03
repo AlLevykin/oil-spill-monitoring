653ad943328e9f1106929284697743a81fb4c39c
AlLevykin <a.a.levykin@gmail.com>
2021-12-03 20:55:06 +0500

    Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
-import { Col, Row, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Spills from '../Spills';
 import { randomNumber } from '../../models/utils';
 import store from '../../store';
@@ -9,44 +9,37 @@ const Sidebar = () => {
 
     useEffect(() => {
         store.dispatch.spills.getSpills();
-      }, []);
+    }, []);
 
     const items = useSelector(state => state.spills);
     const [current, setCurrent] = useState(0);
 
     const clickHandler = () => {
-        setCurrent(randomNumber(0, items.length-1))
+        setCurrent(randomNumber(0, items.length - 1))
         console.log(current);
     }
 
     return (
-        <div className="sidebar position-fixed border border-light shadow rounded-3 ms-5 mt-5 bg-white">
-            <Row>
-                <Col>
-                    <h5 className="m-2 text-secondary text-center">Система мониторинга нефтеразливов</h5>
-                    <hr></hr>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="m-2">
-                    <Button variant="secondary" className="w-100" onClick={clickHandler}>
+        <div className="sidebar position-fixed border border-light shadow rounded-3 ms-5 mt-5 p-2 bg-white">
+            <div className="row mx-2">
+                <div class="col-10">
+                    <h4 className="text-secondary">Данные мониторинга</h4>
+                </div>
+                <div class="col-2">
+                    <Button variant="outline-secondary" onClick={clickHandler}>
                         <span className="mx-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
                                 <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
                             </svg>
                         </span>
-                        Обновить данные мониторинга
                     </Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    {
-                        items.length > 0 ? <Spills spill={items[current]} /> : <></>
-                    }                    
-                </Col>
-            </Row>
+                </div>
+            </div>
+            <hr></hr>
+            {
+                items.length > 0 ? <Spills spill={items[current]} /> : <></>
+            }
         </div>
     )
 }
--- src/components/Spills/index.jsx
+++ src/components/Spills/index.jsx
@@ -1,29 +1,29 @@
-import { Col, Row, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 const Spills = ({ spill }) => {
 
     return (
-        <div className="spills">
-            <form className="mx-2">
-                <fieldset disabled>
-                    <div className="mb-3">
-                        <label className="form-label form-control-sm">Предприятие</label>
-                        <input type="text" className="form-control form-control-sm" value={spill.company} />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label form-control-sm">Лицензионный участок</label>
-                        <Row>
-                            <Col>
-                                <input type="text" className="form-control form-control-sm" value={spill.licenseName} />
-                            </Col>
-                            <Col>
-                                <input type="text" className="form-control form-control-sm" value={spill.licenseId} />
-                            </Col>                            
-                        </Row>
-                    </div>
-                    <legend>Данные мониторинга</legend>
-                </fieldset>
-            </form>
+        <div className="spills px-2">
+            <h5 className="text-secondary">Предприятие</h5>
+            <p>{spill.company}</p>
+            <h5 className="text-secondary">Лицензионный участок</h5>
+            <p>{spill.licenseName} ({spill.licenseId})</p>            
+            <div className="row me-2">
+                <div class="col-10 align-self-center">
+                <h5 className="text-secondary">Вероятное загразнение</h5>
+                    <p>{spill.lat}, {spill.lng}</p>
+                </div>
+                <div class="col-2">
+                    <Button variant="secondary">
+                        <span className="mx-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
+                                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
+                            </svg>
+                        </span>
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
